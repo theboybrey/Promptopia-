20bdf51be72159a9bd1fa71ee5e69bd37540f044
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-09-03 12:09:17 -0400

    Form added

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Link } from "next/link";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text_left">
+        <span className="blue_gradient"> {type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the world, and let your
+        imagaination run wild with any AI-powered platform.
+      </p>
 
-export default Form
+      <form
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+        onSubmit={handleSubmit}
+      >
+        <label></label>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
