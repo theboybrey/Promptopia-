3cf7c2525da49cef621783592fca2eb842dd4e7c
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-09-03 12:52:03 -0400

    Prompt creating

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,4 +1,4 @@
-import { Link } from "next/link";
+import  Link  from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
@@ -15,7 +15,45 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
         onSubmit={handleSubmit}
       >
-        <label></label>
+        <label>
+          <span className="font-satoshi  font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          />
+        </label>
+        <label>
+          <span className="font-satoshi  font-semibold text-base text-gray-700">
+            My Tag {` `}
+            {/* <span className="font-normal">(#web_development, #products, #idea) </span> */}
+          </span>
+
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="#my_prompt_tag"
+            required
+            className="form_input"
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-sm font-semibold text-gray-500 hover:text-gray-700">Cancel </Link>
+
+          <button
+          type="submit"
+          disabled={submitting}
+          className="px-5 py-1.5 text-sm bg-primary-orange rounded-full hover:bg-orange-600 text-white font-semibold"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   );
