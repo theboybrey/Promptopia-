615a20a899f22dcfc1bce2199c695ac1dc2d2544
Lex Benjamin <abessilfie@st.ug.edu.gh>
2023-09-03 11:41:52 -0400

    Updating the App dir

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,30 +1,47 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-import { connectToDB } from "@utils/database";
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    })
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-      // Check if user exists in database
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
 
-      // If user does not exist, create user in database
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      return true;
-    } catch (error) {
-      console.log(error.data.message);
-      return false;
-    }
-  },
-});
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
 
-export { handler as GET, handler as POST };
+        return true
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
+      }
+    },
+  }
+})
+
+export { handler as GET, handler as POST }
--- models/user.js
+++ models/user.js
@@ -11,6 +11,12 @@ const UserSchema = new Schema({
         type: String,
         required: [true, 'Username is required!'],
         match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"],
-
     },
-})
+    image: {
+        type: String,
+    },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
